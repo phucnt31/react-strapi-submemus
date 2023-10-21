8f7c478b551e4e3d36512b626110c5a2d12c2836
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 11:29:21 +0700

    Render navlinks for big screen

--- src/NavLinks.jsx
+++ src/NavLinks.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import sublinks from "./data";
 
 const NavLinks = () => {
-  return <div>NavLinks</div>;
+  return (
+    <div className="nav-links">
+      {sublinks.map((sublink) => {
+        const { page, pageId } = sublink;
+        return (
+          <button className="nav-link" key={pageId}>
+            {page}
+          </button>
+        );
+      })}
+    </div>
+  );
 };
 
 export default NavLinks;
