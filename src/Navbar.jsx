12caf1d5e3936059cd11ff756593098b3134663a
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 12:30:44 +0700

    Fix case when hovering out of navbar the content doesn't disappear and fix import

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { FaBars } from "react-icons/fa";
 import { useGlobalContext } from "./context";
-import NavLinks from "./Navlinks";
+import NavLinks from "./NavLinks";
 
 const Navbar = () => {
-  const { openSidebar } = useGlobalContext();
+  const { openSidebar, setPageId } = useGlobalContext();
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains("nav-link")) setPageId(null);
+  };
 
   return (
     <nav>
-      <div className="nav-center">
+      <div className="nav-center" onMouseOver={handleSubmenu}>
         <h3 className="logo">strapi</h3>
         <button type="button" className="toggle-btn" onClick={openSidebar}>
           <FaBars />
