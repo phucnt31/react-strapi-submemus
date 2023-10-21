92d5ffd79a209269940a989a044984c84bdcd967
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 11:18:03 +0700

    Customize Sidebar.jsx and add toggle sidebar function to Navbar.jsx

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import { useGlobalContext } from "./context";
+import sublinks from "./data";
+import { FaBars } from "react-icons/fa";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className={isSidebarOpen ? "show-sidebar sidebar" : "sidebar"}>
+      <div className="sidebar-container">
+        <button type="button" className="close-btn" onClick={closeSidebar}>
+          <FaBars />
+        </button>
+      </div>
+      <div>
+        {sublinks.map((sublink) => {
+          const { links, page, pageId } = sublink;
+          return (
+            <article key={pageId}>
+              <div className="sidebar-sublinks">
+                <h4>{page}</h4>
+                {links.map((link) => {
+                  const { icon, id, label, url } = link;
+                  return (
+                    <a href={url} key={id}>
+                      {icon} {label}
+                    </a>
+                  );
+                })}
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
