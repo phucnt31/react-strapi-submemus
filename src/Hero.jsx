f7869902b95b36e448526ec0281dbde8db432156
phucnt31 <nguyenthienphuc.02@gmail.com>
2023-10-21 11:02:15 +0700

    Customize Hero component

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <article className="hero-container">
+      <div className="hero-center">
+        <h1>
+          Manage Any Content <br /> Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It’s 100% JavaScript
+          and fully customizable.
+        </p>
+      </div>
+    </article>
+  );
 };
 
 export default Hero;
